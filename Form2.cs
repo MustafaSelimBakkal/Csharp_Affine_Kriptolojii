dc428742515125a5cc2579e8b3e40a99cb3d2b3f
Mustafa Selim Bakkal <130986941+MustafaSelimBakkal@users.noreply.github.com>
2023-06-06 11:01:02 +0300

    Update Form2.cs

--- Form2.cs
+++ Form2.cs
@@ -9,6 +9,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
+// Affine kriptolojide 2 değişken 1 alfabe karakter sayısı vardır 
+// a ve b değişkenleri asal sayı olmak zorunda 
+// m ise alfabe karakteri uygulama türkçe alfabe kullandığı için sabit 29 olmak zourunda
+// y=ax+b denklemi kullanılır. y şifrelenmiş mesaj x ise şifrelenecek mesajdır 
+
 namespace Affine_Kriptoloji
 {
     public partial class Form2 : Form { 
@@ -25,10 +30,10 @@ namespace Affine_Kriptoloji
         private void button1_Click(object sender, EventArgs e)
         {
             a = int.Parse(textBox1.Text);
-            b = int.Parse(textBox2.Text);
+            b = int.Parse(textBox2.Text);     // Değişkenleri alıyoruz
             m = int.Parse(textBox3.Text);
 
-            if (a % m != 0)
+            if (a % m != 0)                   // Sayının asal olup olamdığını kontrol ediyoruz
             {
                 metin = richTextBox1.Text.ToUpper();
                 şifreliMetin = "";
