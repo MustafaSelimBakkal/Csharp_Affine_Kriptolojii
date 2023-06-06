0ea0e16ac73cb02fe4c869bcc40b8ab84e56c874
Mustafa Selim Bakkal <130986941+MustafaSelimBakkal@users.noreply.github.com>
2023-06-06 10:48:06 +0300

    Add files via upload

---
+++ Form2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Affine_Kriptoloji
+{
+    public partial class Form2 : Form { 
+        string alfabe = "ABCÇDEFGĞHIİJKLMNOÖPRSŞTUÜVYZ";
+        int a, b, m, k;
+        string metin, şifreliMetin;
+        string z;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a = int.Parse(textBox1.Text);
+            b = int.Parse(textBox2.Text);
+            m = int.Parse(textBox3.Text);
+
+            if (a % m != 0)
+            {
+                metin = richTextBox1.Text.ToUpper();
+                şifreliMetin = "";
+
+
+                for (int i = 0; i < metin.Length; i++)
+                {
+                    if (alfabe.IndexOf(metin[i]) == -1)
+                    {
+                        şifreliMetin += metin[i].ToString();
+                    }
+
+                    else
+                    {
+
+                        k = alfabe.IndexOf(metin[i]);
+                        z = alfabe[((k * a + b) % m)].ToString();
+
+                        şifreliMetin += z;
+
+                    }
+
+                }
+
+                richTextBox2.Text = şifreliMetin;
+            }
+
+            else
+            {
+                MessageBox.Show("a ve m Değerleri Birbirine Tam Bölünememelidir.");
+            }
+
+
+        }
+    }
+}
